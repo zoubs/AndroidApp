96d7b4b474386fdf474b28d21b1a0d76ad7a721a
GiannisChen <giannischen@foxmail.com>
2020-05-14 20:27:13 +0800

    step 1.2.3:information checked

--- app/src/main/java/com/example/myapplication/ui/info/InfoFragment.java
+++ app/src/main/java/com/example/myapplication/ui/info/InfoFragment.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.share;
+package com.example.myapplication.ui.info;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
 
-public class ShareFragment extends Fragment {
+public class InfoFragment extends Fragment {
 
-    private ShareViewModel shareViewModel;
+    private InfoViewModel infoViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(ShareViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_share, container, false);
+        infoViewModel =
+                ViewModelProviders.of(this).get(InfoViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_info, container, false);
         final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
+        infoViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/myapplication/ui/info/InfoViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/info/InfoViewModel.java
@@ -1,16 +1,16 @@
-package com.example.myapplication.ui.share;
+package com.example.myapplication.ui.info;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class ShareViewModel extends ViewModel {
+public class InfoViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public ShareViewModel() {
+    public InfoViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is share fragment");
+        mText.setValue("This is info fragment");
     }
 
     public LiveData<String> getText() {
